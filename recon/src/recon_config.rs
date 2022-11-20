3045013fea3ba41d0c8a539e05776d6be9c2f24e
wyatt-a <waustin47@outlook.com>
2022-11-20 18:17:22 -0500

    setting up remote connections

--- recon/src/recon_config.rs
+++ recon/src/recon_config.rs
@@ -275,19 +275,17 @@ pub trait RemoteSystem {
                 true
             }
             false => {
-                println!("passwordless connection failed for {} on {}",self.user(), self.hostname());
+                println!("passwordless connection failed for {} on {}", self.user(), self.hostname());
                 false
             }
         }
     }
     fn copy_ssh_key(&self) {
-        println!("enter password for user {} on {}",self.user(),self.hostname());
+        println!("enter password for user {} on {}", self.user(), self.hostname());
         let mut cmd = Command::new("ssh-copy-id");
-        cmd.arg(format!("{}@{}",self.user(),self.hostname()));
-        println!("attempting to run {:?}",cmd);
-        if ! cmd.output().expect("failed to launch ssh-copy-id").status.success() {
-            panic!("you must resolve ssh issues. Maybe you are on a windows system? Sorry if you are");
-        }
+        cmd.arg(format!("{}@{}", self.user(), self.hostname()));
+        println!("attempting to run {:?}", cmd);
+        let o = cmd.output().expect("failed to launch ssh-copy-id");
     }
 }
 
