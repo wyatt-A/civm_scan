14fcf6ddf15f7c6e838011734b7b2b9deeb2ab8f
wyatt-A <waustin47@outlook.com>
2022-12-03 16:13:53 -0500

    update

--- recon/src/main.rs
+++ recon/src/main.rs
@@ -19,7 +19,7 @@ pub struct ReconArgs {
 #[derive(clap::Subcommand,Debug)]
 pub enum ReconAction {
     /// reconstruct a diffusion-weighted series of volumes
-    Dti(DtiRecon),
+    Dti(NewRecon),
     /// check the status of a reconstruction by run number
     Status(RunnoArgs),
     /// restart a recon by run number
@@ -66,7 +66,7 @@ pub struct RestartArgs {
     #[clap(long)]
     forced_state:Option<String>,
     #[clap(long,short)]
-    skip_send_to_engine:Option<bool>,
+    send_to_engine:Option<bool>,
 }
 
 #[derive(Clone,clap::Args,Debug)]
@@ -89,7 +89,7 @@ pub struct RunnoArgs {
 }
 
 #[derive(Clone,clap::Args,Debug)]
-pub struct DtiRecon {
+pub struct NewRecon {
     /// civm id (cof,wa41,kjh ...)
     civm_id:String,
     /// base configuration used to define recon parameters
@@ -156,69 +156,251 @@ fn main() {
         }
         ReconAction::Restart(args) => restart(args),
         ReconAction::Status(args) => status(args),
-        ReconAction::Dti(args) => dti(args),
+        ReconAction::Dti(args) => _dti(args),
         ReconAction::Cancel(args) => cancel(args),
         ReconAction::WaitForCompletion(args) => wait_for_completion(args),
     }
 }
 
-fn restart(args:RestartArgs){
-    let bg = std::env::var("BIGGUS_DISKUS").expect("BIGGUS_DISKUS must be set on this workstation");
-    let engine_work_dir = Path::new(&bg);
-    let work_dir = engine_work_dir.join(format!("{}.work",&args.run_number));
-    // find all volume manager config files recursively
-    let config_files = utils::find_files(&work_dir,"volman_config");
-    let mut n_restarted = 0;
-    match config_files {
-        Some(mut files) => {
-            files.sort();
-            for config_file in files.iter() {
-                let mut c = VolumeManagerConfig::from_file(config_file);
-                if args.disable_slurm.is_some() { c.slurm_disabled = args.disable_slurm.unwrap(); }
-                if args.skip_send_to_engine.is_some() { c.send_to_engine = args.skip_send_to_engine.unwrap(); }
-                c.to_file(config_file);
-                /*
-                    If we see a volume manager (vm) state file, and we are forcing a particular state,
-                    set the state of the vm and write the state file back to disk. Then, if the volume manager
-                    is not in the "Done" state, cancel the existing job and relaunch it.
-                 */
-                let mut vm = VolumeManager::read(config_file);
-                match vm {
-                    Some(mut vm) => {
-                        match &args.forced_state {
-                            Some(state) => {
-                                vm.set_state(state);
-                                vm.to_file();
-                            }
-                            _=> {}
-                        }
-                        match vm.state(){
-                            VolumeManagerState::Done => {},
-                            _=> {
-                                match c.is_slurm_disabled() {
-                                    true => {VolumeManager::launch(config_file);}
-                                    false => {
-                                        vm.cancel_job();
-                                        VolumeManager::launch_with_slurm_now(config_file);
-                                    }
-                                }
-                                n_restarted += 1;
-                            }
-                        }
-                    }
-                    _=> {}
-                }
-            }
-            status(RunnoArgs{
-                run_number:args.run_number.clone()
-            });
-            println!("restarted {} volume managers.",n_restarted);
-        },
-        None => {
-            println!("no volume manager configs found in {:?}",work_dir);
+
+/*
+    Do stuff to a collection of volumes managers. This may launching, relaunching, configurations ...
+ */
+
+#[derive(Clone,Debug)]
+pub enum VMCollectionType {
+    /// volume managers are operating on a collection of mr raw files leading to a collection
+    /// of volume managers
+    Dti,
+    /// volume managers are operating on a single mr raw file from the scanner, but require multiple volumes
+    /// to be reconstructed
+    MultiEcho,
+    /// a single volume is to be reconstructed
+    Single,
+}
+
+
+pub struct VolumeManagerCollection {
+    work_dir:PathBuf,
+    vm_config_files:Vec<PathBuf>
+}
+
+impl VolumeManagerCollection {
+    pub fn new(collection_type:VMCollectionType,settings:&NewRecon,work_dir:&Path) -> Self {
+        use VMCollectionType::*;
+        let cfgs = match &collection_type {
+            // handle initial setup
+            Dti => VolumeManagerConfig::new_dti_config(
+                    &settings.project_settings,
+                    &settings.civm_id,
+                    &settings.run_number,
+                    &settings.specimen_id,
+                    &settings.raw_data_base_dir
+                ),
+            _=> panic!("{:?} not yet implemented for volume manager collection!",collection_type)
+        };
+
+        // trim configs down if a subset is specified
+        let user_last_vol = settings.last_volume.unwrap_or(cfgs.len()-1);
+        let upper_index = if user_last_vol >= cfgs.len() {cfgs.len()-1} else {user_last_vol};
+        let user_first_vol = settings.first_volume.unwrap_or(0);
+        let lower_index = if user_first_vol >= cfgs.len() {cfgs.len()-1} else {user_first_vol};
+        let mut cfgs = Vec::from_iter(cfgs[lower_index..(upper_index +1)].iter().cloned());
+        //println!("launching recon for range {} to {}. {} volumes will be launched for reconstruction", lower_index, upper_index, upper_index - lower_index +1);
+        Self::_apply_settings_new(&mut cfgs,settings,work_dir);
+        let cfg_files = Self::to_disk(&cfgs,work_dir);
+        Self {
+            work_dir:work_dir.to_owned(),
+            vm_config_files:cfg_files
         }
     }
+
+    pub fn from_work_dir(work_dir:&Path) -> Option<Self> {
+        let cfg_files = utils::find_files(work_dir,"volman_config")?;
+        Some(Self{
+            work_dir: work_dir.to_owned(),
+            vm_config_files: cfg_files,
+        })
+    }
+
+    pub fn launch(&self) {
+        self.vm_config_files.iter().for_each(|file|{
+            let cfg = VolumeManagerConfig::from_file(file);
+            match cfg.is_slurm_disabled() {
+                true => {
+                    println!("launching volume manager without slurm {:?}",file);
+                    VolumeManager::launch(file);
+                },
+                false => {
+                    let jid = VolumeManager::launch_with_slurm_now(file);
+                    println!("{} job submitted with id {}",cfg.name(),jid);
+                }
+            }
+        });
+    }
+
+    pub fn cancel(&self) {
+        let mut n_cancelled = 0;
+        self.vm_config_files.iter().for_each(|file| {
+            match VolumeManager::read(file) {
+                Some(vm) => {
+                    if vm.cancel_job(){
+                        n_cancelled += 1;
+                    }
+                },
+                None => {}
+            }
+        });
+        println!("{} volume managers cancelled",n_cancelled);
+    }
+
+    pub fn restart(&self) {
+        self.cancel();
+        self.launch();
+    }
+
+    pub fn force_state(&self,state:&str) {
+        self.vm_config_files.iter().for_each(|file|{
+            match VolumeManager::read(file){
+                Some(mut vm) => {
+                    vm.set_state(state);
+                    vm.to_file();
+                },
+                None => {}
+            }
+        });
+    }
+
+    pub fn incomplete(&self) -> Self {
+        let incomplete = self.vm_config_files.iter().filter(|file|{
+            match VolumeManager::read(file){
+                Some(vm) => !vm.is_done(),
+                None => true
+            }
+        }).map(|file| file.to_owned()).collect();
+        Self {
+            work_dir: self.work_dir.clone(),
+            vm_config_files: incomplete,
+        }
+    }
+
+    fn vm_configs(&self) -> Vec<VolumeManagerConfig>{
+        self.vm_config_files.iter().map(|file| VolumeManagerConfig::from_file(file)).collect()
+    }
+
+    fn to_disk(cfgs:&Vec<VolumeManagerConfig>,work_dir:&Path) -> Vec<PathBuf> {
+        cfgs.iter().map(|cfg|{
+            let config_path = work_dir.join(cfg.name());
+            create_dir_all(&config_path).expect(&format!("unable to create {:?}",config_path));
+            let file = config_path.join(cfg.name());
+            cfg.to_file(&file);
+            file
+        }).collect()
+    }
+
+    fn from_disk(&self) -> Vec<VolumeManagerConfig> {
+        // find volume managers in work dir
+        self.vm_config_files.iter().map(|file| VolumeManagerConfig::from_file(file)).collect()
+    }
+
+    fn _apply_settings_new(cfgs:&mut Vec<VolumeManagerConfig>,settings:&NewRecon,work_dir:&Path){
+        cfgs.iter_mut().for_each(|cfg|{
+            cfg.slurm_disabled = settings.disable_slurm.unwrap_or(false);
+            cfg.send_to_engine = settings.send_to_engine.unwrap_or(true);
+            cfg.vm_settings.engine_work_dir = work_dir.to_owned();
+        });
+        Self::to_disk(&cfgs,work_dir);
+    }
+
+    pub fn apply_settings_new(&mut self,settings:&NewRecon) {
+        let mut cfgs = self.from_disk();
+        Self::_apply_settings_new(&mut cfgs,settings,&self.work_dir);
+    }
+
+    pub fn apply_settings_restart(&mut self,settings:&RestartArgs){
+        let mut cfgs = self.from_disk();
+        cfgs.iter_mut().for_each(|cfg|{
+            cfg.slurm_disabled = settings.disable_slurm.unwrap_or(false);
+            cfg.send_to_engine = settings.send_to_engine.unwrap_or(true);
+        });
+        Self::to_disk(&cfgs,&self.work_dir);
+    }
+
 }
+
+
+
+
+
+
+// fn restart(args:RestartArgs){
+//     let bg = std::env::var("BIGGUS_DISKUS").expect("BIGGUS_DISKUS must be set on this workstation");
+//     let engine_work_dir = Path::new(&bg);
+//     let work_dir = engine_work_dir.join(format!("{}.work",&args.run_number));
+//     // find all volume manager config files recursively
+//     let config_files = utils::find_files(&work_dir,"volman_config");
+//     let mut n_restarted = 0;
+//     match config_files {
+//         Some(mut files) => {
+//             files.sort();
+//             for config_file in files.iter() {
+//                 let mut c = VolumeManagerConfig::from_file(config_file);
+//                 if args.disable_slurm.is_some() { c.slurm_disabled = args.disable_slurm.unwrap(); }
+//                 if args.send_to_engine.is_some() { c.send_to_engine = args.send_to_engine.unwrap(); }
+//                 c.to_file(config_file);
+//                 /*
+//                     If we see a volume manager (vm) state file, and we are forcing a particular state,
+//                     set the state of the vm and write the state file back to disk. Then, if the volume manager
+//                     is not in the "Done" state, cancel the existing job and relaunch it.
+//                  */
+//                 let mut vm = VolumeManager::read(config_file);
+//                 match vm {
+//                     Some(mut vm) => {
+//                         match &args.forced_state {
+//                             Some(state) => {
+//                                 vm.set_state(state);
+//                                 vm.to_file();
+//                             }
+//                             _=> {}
+//                         }
+//                         match vm.state(){
+//                             VolumeManagerState::Done => {},
+//                             _=> {
+//                                 match c.is_slurm_disabled() {
+//                                     true => {VolumeManager::launch(config_file);}
+//                                     false => {
+//                                         vm.cancel_job();
+//                                         VolumeManager::launch_with_slurm_now(config_file);
+//                                     }
+//                                 }
+//                                 n_restarted += 1;
+//                             }
+//                         }
+//                     }
+//                     _=> {}
+//                 }
+//             }
+//             status(RunnoArgs{
+//                 run_number:args.run_number.clone()
+//             });
+//             println!("restarted {} volume managers.",n_restarted);
+//         },
+//         None => {
+//             println!("no volume manager configs found in {:?}",work_dir);
+//         }
+//     }
+// }
+
+
+fn restart(args:RestartArgs){
+    let work_dir = work_dir_big_disk(&args.run_number);
+    let mut vm_collection = VolumeManagerCollection::from_work_dir(&work_dir).expect(&format!("no volume manager configs found in {:?}",work_dir));
+    vm_collection.apply_settings_restart(&args);
+    vm_collection.incomplete().restart();
+}
+
+
+
 
 fn cancel(args:RunnoArgs) {
     println!("finding jobs to cancel for {} ...",args.run_number);
@@ -261,11 +443,16 @@ fn cancel(args:RunnoArgs) {
 }
 
 
-fn status(args:RunnoArgs) {
-    println!("running recon status check on {} ...",args.run_number);
+fn work_dir_big_disk(run_number:&str) -> PathBuf {
     let bg = std::env::var("BIGGUS_DISKUS").expect("BIGGUS_DISKUS must be set on this workstation");
     let engine_work_dir = Path::new(&bg);
-    let work_dir = engine_work_dir.join(format!("{}.work",&args.run_number));
+    engine_work_dir.join(format!("{}.work",run_number))
+}
+
+
+fn status(args:RunnoArgs) {
+    println!("running recon status check on {} ...",args.run_number);
+    let work_dir = work_dir_big_disk(&args.run_number);
 
     if !work_dir.exists(){
         println!("{} not found. {:?} doesn't exist.",args.run_number,work_dir);
@@ -385,8 +572,58 @@ fn slurm_recon_watch(args:WaitForCompletionArgs,email:&str) {
 }
 
 
+fn _dti(args: NewRecon) {
+    let bg = std::env::var("BIGGUS_DISKUS").expect("BIGGUS_DISKUS must be set on this workstation");
+    let engine_work_dir = Path::new(&bg);
 
-fn dti(args:DtiRecon){
+
+    // Test connections to scanner and archive engine
+    let p = ProjectSettings::from_file(&args.project_settings);
+
+    if !p.archive_info.is_valid(&p.project_code,&args.civm_id) {
+        match args.no_archive.unwrap_or(false) {
+            false => {
+                println!("project meta data is incorrect for archiving. You must repair the project settings at {:?} and try again.",args.project_settings);
+                println!("if you want to run the recon anyway, re-run with the no archive option. Use --help to find it.");
+                return
+            }
+            true => {
+                println!("you have opted into running the recon despite not passing the meta data validation for archiving!");
+            }
+        }
+    }
+
+    if args.send_to_engine.unwrap_or(true) && !p.archive_engine_settings.test_connection(){
+        println!("you must fix the remote connection");
+        return
+    }
+
+    if !p.scanner_settings.test_connection() {
+        println!("you must fix the remote connection");
+        return
+    }
+
+    let work_dir = work_dir_big_disk(&args.run_number);
+    let vm_collection = VolumeManagerCollection::new(VMCollectionType::Dti,&args,&work_dir);
+
+    if !args.batch_mode.unwrap_or(false) {
+        let mut user_in = String::new();
+        println!("----------------SETTINGS----------------");
+        println!("project_file = '{:?}'",args.project_settings);
+        println!("specimen_id = '{}'",args.specimen_id);
+        println!("{}",p.to_txt());
+        println!("----------------------------------------");
+        println!("is this configuration correct? Hit enter to continue or control-C to cancel");
+        stdin().read_line(&mut user_in).expect("provide an input!");
+    }
+
+    vm_collection.launch();
+
+}
+
+
+
+fn dti(args: NewRecon){
     // Where are we going to live?
     let bg = std::env::var("BIGGUS_DISKUS").expect("BIGGUS_DISKUS must be set on this workstation");
     let engine_work_dir = Path::new(&bg);
@@ -416,6 +653,7 @@ fn dti(args:DtiRecon){
         println!("you must fix the remote connection");
         return
     }
+
 
     // Generate a vector of volume manager configurations that will be operated on
     let mut vm_configs = recon_config::VolumeManagerConfig::new_dti_config(
