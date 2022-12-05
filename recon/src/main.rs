dedb7cb217a1b20076179bcefa080c6b9716f178
wyatt-a <wyatt-a@users.noreply.github.com>
2022-12-05 12:15:20 -0500

    update

--- recon/src/main.rs
+++ recon/src/main.rs
@@ -283,6 +283,7 @@ impl VolumeManagerCollection {
     pub fn restart(&self) {
         self.cancel();
         self.launch();
+        println!("restarted {} volume managers",self.vm_config_files.len());
     }
 
     pub fn force_state(&self,state:&str) {
